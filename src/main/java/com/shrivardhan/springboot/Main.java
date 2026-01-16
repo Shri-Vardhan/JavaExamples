82c252b6ed1ff23ccc9c006e8db074bdf225f448
Shrivardhan <chshrivardhan@gmail.com>
2026-01-16 22:12:57 +0530

    updated with profile logic

--- src/main/java/com/shrivardhan/springboot/Main.java
+++ src/main/java/com/shrivardhan/springboot/Main.java
@@ -7,11 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.Connection;
 
-@SpringBootApplication(scanBasePackages = {"com.shrivardhan.springboot", "com.shrivardhan.connector"})
+@SpringBootApplication(scanBasePackages = {"com.shrivardhan"})
 public class Main implements CommandLineRunner {
 
     @Autowired
     private DBconnection db;
+
+    @Autowired
+    private org.springframework.core.env.Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
@@ -19,6 +22,12 @@ public class Main implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Active profiles: " +
+                java.util.Arrays.toString(environment.getActiveProfiles()));
+        System.out.println("URL = " + environment.getProperty("spring.datasource.url"));
+        System.out.println("Username = " + environment.getProperty("spring.datasource.username"));
+        System.out.println("Password = " + environment.getProperty("spring.datasource.password"));
+
         Connection con = db.getConnection();
         System.out.println("Connected to DB: " + con);
         con.close();
