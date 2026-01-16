f31a54c1eb51e3795e349fac3ff100863d7bc392
Shrivardhan <chshrivardhan@gmail.com>
2026-01-16 12:17:39 +0530

    spring boot working at this point

---
+++ src/main/java/com/shrivardhan/springboot/DBconnection.java
@@ -0,0 +1,21 @@
+package com.shrivardhan.springboot;
+
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+public class DBconnection {
+
+    private final DataSource dataSource;
+
+    public DBconnection(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
