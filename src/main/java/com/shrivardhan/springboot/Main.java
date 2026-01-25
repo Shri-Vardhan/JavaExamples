5cd454b41a27b7401dc1d5256367f2780ecd9348
Shrivardhan <chshrivardhan@gmail.com>
2026-01-25 10:43:20 +0530

    implemented with Apache log4j

--- src/main/java/com/shrivardhan/springboot/Main.java
+++ src/main/java/com/shrivardhan/springboot/Main.java
@@ -1,5 +1,7 @@
 package com.shrivardhan.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
@@ -10,24 +12,23 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
+    private static final Logger logger = LogManager.getLogger(Main.class);
     private final JdbcTemplate jdbcTemplate;
     private final Environment environment;
-
 
     public Main(JdbcTemplate jdbcTemplate, Environment environment) {
         this.jdbcTemplate = jdbcTemplate;
         this.environment = environment;
         String sql = "SELECT * FROM TAB";
 
-        System.out.println("URL = " + environment.getProperty("spring.datasource.url"));
-        System.out.println("Username = " + environment.getProperty("spring.datasource.username"));
-        System.out.println("Password = " + environment.getProperty("spring.datasource.password"));
+        logger.info("Datasource URL = {}", environment.getProperty("spring.datasource.url"));
+        logger.info("Datasource Username = {}", environment.getProperty("spring.datasource.username"));
 
-        try {  
+        try {
             jdbcTemplate.execute(sql);
+            logger.info("Database connection successful");
         } catch (Exception exception) {
-            System.out.println("Connection error");
+            logger.error("Database connection failed", exception);
         }
     }
 
