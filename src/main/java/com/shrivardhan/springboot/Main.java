c5df636408428c1c7697a9e9473afc996d18eec4
Shrivardhan <chshrivardhan@gmail.com>
2026-01-25 09:39:08 +0530

    Applied log4j logging

--- src/main/java/com/shrivardhan/springboot/Main.java
+++ src/main/java/com/shrivardhan/springboot/Main.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    private static final Logger logger =
+            LogManager.getLogger(Main.class);
 
     private final JdbcTemplate jdbcTemplate;
     private final Environment environment;
@@ -20,14 +25,17 @@ public class Main {
         this.environment = environment;
         String sql = "SELECT * FROM TAB";
 
+        logger.info("Running");
+
         System.out.println("URL = " + environment.getProperty("spring.datasource.url"));
         System.out.println("Username = " + environment.getProperty("spring.datasource.username"));
         System.out.println("Password = " + environment.getProperty("spring.datasource.password"));
 
-        try {  
+        try {
             jdbcTemplate.execute(sql);
+            logger.info("Data fetched successfully");
         } catch (Exception exception) {
-            System.out.println("Connection error");
+            logger.error("Database error occurred", exception);
         }
     }
 
